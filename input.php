c4a7bae88d8ffa0d70b018ce8083b62b433fb8d9
pascalkienast <pascal.kienast9@gmail.com>
2015-10-17 00:54:29 +0200

    Es nimmt langsam Form an!

--- input.php
+++ input.php
@@ -1,7 +1,22 @@
 <?php
+// Connect to uberspace Database
 $db = new mysqli('localhost', 'kibl', 'dighlanAwpyinWieco', 'kibl_ude');
 
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
+
+// create unique id like 4b3403665fea6 
+$uid = uniqid();
+
+//table
+$sql = "CREATE TABLE ".$uid."
+(
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+long FLOAT(32),
+lat FLOAT(32)
+)";
+
+$db->query($sql);
+
 ?>
